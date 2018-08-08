58e189cf9629995778b524501a5ee92bd0dc9ed0
Mohammed Sohail Sameja <sohailsameja@gmail.com>
2018-08-08 23:32:34 +0530

    Working prototype
    - Added Rate-limiting
    - Streaming chunks

--- cli.js
+++ cli.js
@@ -6,14 +6,3 @@ const processor = require("./lib/processor");
 inquirer.prompt(flow.cliFlow).then(cliRes => {
     return processor.init(cliRes);
 });
-
-/*req({
-    method: "POST",
-    uri: `https://old.reddit.com/api/login/${loginDetails.user}`,
-    form: loginDetails
-}).then(res => {
-    const sessionCookie = JSON.parse(res).json.data.cookie
-    console.log(sessionCookie);
-}).catch(error => {
-    console.error(error);
-});*/
